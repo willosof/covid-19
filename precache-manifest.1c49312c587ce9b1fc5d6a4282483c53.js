6edf8f7e4b2d3c1b7acaea086312d6c64ba9bd90
William Viker <william.viker@gmail.com>
2020-03-26 01:29:01 +0100

    Updates

---
+++ precache-manifest.1c49312c587ce9b1fc5d6a4282483c53.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aef3732f9feb5b1f91d97c85ffb0faee",
+    "url": "/covid-19/index.html"
+  },
+  {
+    "revision": "92cb05723fe07ccb5c2e",
+    "url": "/covid-19/static/css/2.83342c2a.chunk.css"
+  },
+  {
+    "revision": "ed91727169a3ae454786",
+    "url": "/covid-19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "92cb05723fe07ccb5c2e",
+    "url": "/covid-19/static/js/2.1f3f793a.chunk.js"
+  },
+  {
+    "revision": "e18637604789af6a40503615de5bd745",
+    "url": "/covid-19/static/js/2.1f3f793a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed91727169a3ae454786",
+    "url": "/covid-19/static/js/main.19579f69.chunk.js"
+  },
+  {
+    "revision": "e84dd164dd3b06d1310b",
+    "url": "/covid-19/static/js/runtime-main.6ca52c11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid-19/static/media/logo.5d5d9eef.svg"
+  }
+]);
