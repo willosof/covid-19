471772be2aa6d1a72e4ee9eb33f9fc046c821eb0
William Viker <william.viker@gmail.com>
2020-03-26 01:26:26 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap-grid.css'
+
+import '@blueprintjs/core/lib/css/blueprint.css';
+
+import MapChart from './Components/MapChart';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MapChart 
+        datalink="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv" 
+        title="CSSE COVID-19: Confirmed (per capita)"
+        min={0}
+        max={50}
+        intent="warning"
+      />
+      <MapChart 
+        datalink="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv" 
+        title="CSSE COVID-19: Deaths (per capita)"
+        min={0}
+        max={10}
+        intent="danger"
+      />
+
     </div>
   );
 }
---
+++ src/Components/MapChart.js
@@ -0,0 +1,168 @@
+import React, { Fragment } from "react";
+import { csv } from "d3-fetch";
+import { scaleLinear } from "d3-scale";
+import { Container, Row, Col } from 'reactstrap';
+import {
+    ComposableMap,
+    Geographies,
+    Geography,
+    Sphere,
+    Graticule
+} from "react-simple-maps";
+import { Slider,Intent,Label } from '@blueprintjs/core';
+
+const geoUrl = "https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json";
+
+export default class MapChart extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            dates: [],
+            max: 10,
+            dateIndex: 0,
+            date: '3/24/20'
+        }
+    }
+
+    componentDidMount() {
+        csv(this.props.datalink).then(data => {
+            data = this.mergeRegions(data);
+            let firstrow = Object.keys(data[0])
+            //console.log("firstrow", data[1]);
+            for (let n = 1; n <= 4; n++) firstrow.shift()
+            firstrow.pop();
+            this.setState({ data, dateIndex: 0, date: firstrow[0], dates: [ ...firstrow ] });
+        });
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        this.colorScale = scaleLinear().domain([this.props.min,nextState.max]).range(["#eee","#f00"]);
+    }
+
+    mergeRegions(data) {
+        
+        let newPreData = {}
+        let manageKeys
+
+        data.map( row => {
+            row.Name = this.nameFix(row['Country/Region']);
+            if (manageKeys === undefined) {
+                manageKeys = Object.keys(row).filter(find => (find !== 'Province/State' && find !== 'Name' && find !== 'Lat' && find !== 'Long'))
+            }
+            if (newPreData[row.Name] === undefined) {
+                newPreData[row.Name] = row;
+            }
+            else {
+                manageKeys.map(key => {
+                    newPreData[row.Name][key] = parseInt(newPreData[row.Name][key])
+                    newPreData[row.Name][key] += parseInt(row[key])
+                })
+            }
+        })
+        
+        return Object.values(newPreData)
+    }
+
+    nameFix(input) {
+        let conv = {
+            'US': 'United States',
+            'Greenland': 'Denmark',
+            'Congo (Brazzaville)': 'Dem. Rep. Congo',
+            'Congo (Kinshasa)': 'Dem. Rep. Congo',
+            'Korea, South' : 'South Korea',
+        };
+        return conv[input] !== undefined ? conv[input] : input
+    }
+
+    changeDate(val) {
+        this.setState({ dateIndex: val, date: this.state.dates[val] })
+    }
+    perCapita(geo, count) {
+        return 100000 / geo.properties.POP_EST * count;
+    }
+    deadCountryColor(geo) {
+        //console.log("wtf is", geo.properties.NAME);
+        return "#ddd";
+    }
+    render() {
+        const data = this.state.data;
+        if (this.state.dates.length === 0) return <Fragment />
+
+        //console.log("state", this.state);
+        return (
+            
+            <Fragment>
+                <Container>
+
+                    <h1>{this.props.title}</h1>
+                    <Row>
+                        <Col md={8}>
+                            <Label>
+                                Time
+                                <Slider
+                                    min={0}
+                                    max={this.state.dates.length-1}
+                                    stepSize={1}
+                                    labelStepSize={10}
+                                    intent={this.props.intent}
+                                    onChange={ val => this.changeDate(val) }
+                                    labelRenderer={ val => this.state.dates[val] }
+                                    showTrackFill={true}
+                                    value={this.state.dateIndex}
+                                    vertical={false}
+                                />
+                            </Label>
+                        </Col>
+                        <Col md={4}>
+                            <Label>
+                                Strength
+                                <Slider
+                                    min={1}
+                                    max={400}
+                                    stepSize={1}
+                                    labelStepSize={25}
+                                    intent={Intent.SUCCESS}
+                                    onChange={ max => this.setState({max}) }
+                                    labelRenderer={ val => parseInt((val/400)*100) }
+                                    showTrackFill={false}
+                                    value={this.state.max}
+                                    vertical={false}
+                                />
+                            </Label>
+                        </Col>
+                    </Row>
+
+                </Container>
+                <Container fluid style={{marginTop: 'calc((-100% / 12)', marginBottom: 'calc((-100% / 12)' }}>
+                    <ComposableMap
+                        projectionConfig={{
+                            rotate: [-10, 0, 0],
+                            scale: 147
+                        }}
+                    >
+                        <Sphere stroke="#E4E5E6" strokeWidth={1.4} />
+                        <Graticule stroke="#E4E5E6" strokeWidth={0.5} />
+
+                        {data.length > 0 && (
+                            <Geographies geography={geoUrl}>
+                                {({ geographies }) =>
+                                    geographies.map(geo => {
+                                        const d = data.find(s => (this.nameFix(s.Name) === geo.properties.NAME || this.nameFix(s.Name) === geo.properties.NAME_LONG) );
+                                        return (
+                                            <Geography
+                                                key={geo.rsmKey}
+                                                geography={geo}
+                                                fill={d ? this.colorScale(this.perCapita(geo, d[this.state.date])) : this.deadCountryColor(geo) }
+                                            />
+                                        );
+                                    })
+                                }
+                            </Geographies>
+                        )}
+                    </ComposableMap>
+                </Container>
+            </Fragment>
+        )
+    }
+}
